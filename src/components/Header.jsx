34e418954f9cdb0228956d855ede564a0dca73c9
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-04-23 20:51:23 +0300

    добавил модальное окно  и настроил его из react-modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,71 +1,91 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-import Modal from "react-modal";
-import { useState } from "react";
-import { IoIosAddCircleOutline } from "react-icons/io";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { IoIosAddCircleOutline } from 'react-icons/io';
+import { HiCamera } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Header = () => {
 	const { data: session } = useSession();
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<header className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
-			<div className="flex justify-between items-center max-w-6xl mx-auto">
+		<header className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
+			<div className='flex justify-between items-center max-w-6xl mx-auto'>
 				{/*Logo */}
 
-				<Link href="/" className="hidden lg:inline-flex">
+				<Link href='/' className='hidden lg:inline-flex'>
 					<Image
-						src="/Instagram_logo_black.webp"
+						src='/Instagram_logo_black.webp'
 						width={96}
 						height={96}
-						alt="logo instagram"
+						alt='logo instagram'
+						priority={true}
 					/>
 				</Link>
-				<Link href="/" className="lg:hidden">
-					<Image src="/logo.webp" width={38} height={38} alt="logo instagram" />
+				<Link href='/' className='lg:hidden'>
+					<Image src='/logo.webp' width={38} height={38} alt='logo instagram' />
 				</Link>
 
 				{/*search */}
 
 				<input
-					type="text"
-					placeholder="поиск..."
-					className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-5 max-w-[170px] sm:max-w-[210px]"
+					type='text'
+					placeholder='поиск...'
+					className='bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-5 max-w-[170px] sm:max-w-[210px]'
 				/>
 
 				{/*menu items */}
 
 				{session ? (
-					<div className="flex gap-6 items-center">
+					<div className='flex gap-6 items-center'>
 						<IoIosAddCircleOutline
-							className="text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600"
+							className='text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600'
 							onClick={() => setIsOpen(true)}
 						/>
 						<img
 							src={session.user.image}
-							alt="avatar user"
-							className="h-10 w-10 rounded-full cursor-pointer"
+							alt='avatar user'
+							className='h-10 w-10 rounded-full cursor-pointer'
 							onClick={signOut}
 						/>
 					</div>
 				) : (
 					<button
 						onClick={signIn}
-						className="text-sm font-semibold text-blue-500"
+						className='text-sm font-semibold text-blue-500'
 					>
 						Вход
 					</button>
 				)}
 			</div>
 			{isOpen && (
-				<Modal isOpen={isOpen}>
-					<div>
-						<h1>Modal</h1>
-						<button onClick={() => setIsOpen(false)}>close</button>
+				<Modal
+					isOpen={isOpen}
+					className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md'
+					onRequestClose={() => setIsOpen(false)}
+					ariaHideApp={false}
+				>
+					<div className='flex flex-col justify-center items-center h-[100%]'>
+						<HiCamera className='text-5xl text-gray-400 cursor-pointer' />
 					</div>
+					<input
+						type='text'
+						maxLength='150'
+						placeholder='Введите название...'
+						className='m-4 border-none text-center w-full focus:ring-0 outline-none'
+					/>
+					<button className='w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100'>
+						Загрузить
+					</button>
+					<AiOutlineClose
+						className='cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300'
+						onClick={() => setIsOpen(false)}
+					/>
 				</Modal>
 			)}
 		</header>
